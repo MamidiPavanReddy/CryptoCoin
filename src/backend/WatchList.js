fc66da901df921fb53c3e214630aff01bb61b4de
Mamidi Pavan Reddy <PavanReddy2802@gmail.com>
2025-02-26 14:28:05 -0500

    Updated Watchlist and Crypto Dashboard components

--- src/backend/WatchList.js
+++ src/backend/WatchList.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-
 const watchItemSchema = new mongoose.Schema({
   symbol: { type: String, required: true },
+  name: { type: String, required: true },  
+  id: { type: String, required: true },    
   dateCreated: { type: Date, default: Date.now }
 });
 
@@ -10,17 +11,21 @@ const WatchItem = mongoose.model('WatchItem', watchItemSchema);
 
 module.exports = {
 
-  addItem: async (symbol) => {
-    const item = new WatchItem({ symbol });
+  // add watchlist items
+  addItem: async (coin) => {
+    const { symbol, name, id } = coin;
+    const item = new WatchItem({ symbol, name, id });
     await item.save();
     return item;
   },
 
+  // Remove item by symbol
   removeItem: async (symbol) => {
     const result = await WatchItem.deleteOne({ symbol });
     return result.deletedCount > 0;
   },
-  
+
+  // Get all items in the watchlist
   getItems: async () => {
     return await WatchItem.find({});
   }
--- src/backend/server.js
+++ src/backend/server.js
@@ -7,18 +7,14 @@ const watchlist = require('./WatchList');
 const app = express();
 const PORT = 3000;
 
-
 app.use(cors());
-
-
 app.use(bodyParser.json());
-
 
 connectDB();
 
-
 const router = express.Router();
 
+// Fetch watchlist 
 router.get('/watchlist', async (req, res) => {
   try {
     const items = await watchlist.getItems();
@@ -28,20 +24,22 @@ router.get('/watchlist', async (req, res) => {
   }
 });
 
+// Add to watchlist 
 router.post('/watchlist', async (req, res) => {
-  const { symbol } = req.body;
-  if (!symbol) {
-    return res.status(400).json({ error: 'Symbol is required' });
+  const { symbol, name, id } = req.body; 
+  if (!symbol || !name || !id) {
+    return res.status(400).json({ error: 'Symbol, Name, and ID are required' });
   }
 
   try {
-    await watchlist.addItem(symbol);
+    await watchlist.addItem({ symbol, name, id });  
     res.status(201).send();
   } catch (err) {
     res.status(500).json({ error: 'Failed to add item to watchlist' });
   }
 });
 
+// Remove from watchlist
 router.delete('/watchlist', async (req, res) => {
   const { symbol } = req.body;
   if (!symbol) {
@@ -59,7 +57,6 @@ router.delete('/watchlist', async (req, res) => {
     res.status(500).json({ error: 'Failed to remove item from watchlist' });
   }
 });
-
 
 app.use(router);
 
